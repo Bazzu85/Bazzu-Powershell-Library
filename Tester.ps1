844462bfc5f1e899c2820d52de5a4f0000808ecd
Cristian Bazzucco <bazzu85.dev@gmail.com>
2023-04-17 22:33:01 +0200

    v1.1

--- Tester.ps1
+++ Tester.ps1
@@ -1,7 +1,9 @@
-. '.\src\Logging\Logging.ps1'
-. '.\src\Create missing directories\Create Missing Directories.ps1'
+$LoggingPS1 = Invoke-WebRequest "https://raw.githubusercontent.com/Bazzu85/Bazzu-Powershell-Library/master/src/Logging/Logging.ps1"
+Invoke-Expression $LoggingPS1
+$CreateMissingDirectoriesPS1 = Invoke-WebRequest "https://raw.githubusercontent.com/Bazzu85/Bazzu-Powershell-Library/master/src/Create%20missing%20directories/Create%20Missing%20Directories.ps1"
+Invoke-Expression $CreateMissingDirectoriesPS1
 
-CheckDirectories $PSScriptRoot
+CheckDirectories 
 
 #LogWrite 
 LogWriteDebug -LogString "A" -MainScriptPath $PSScriptRoot -DebugFlag $true
